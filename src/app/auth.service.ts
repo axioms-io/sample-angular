4d360d487b5f0fa185c42114dded37497948a044
Abhishek Tiwari <abhishek.twr@gmail.com>
2020-03-03 23:45:32 +1100

    Build working but not functional

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth } from '@axioms/web-js';
+import Auth from '@axioms/web-js';
 import { environment } from '../environments/environment';
 import config from '../../config.json';
 
