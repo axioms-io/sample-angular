d6d371c143d5e2381b642d2c265f2988de5d655c
Abhishek Tiwari <abhishek.twr@gmail.com>
2020-03-04 01:08:42 +1100

    Auth import in dev fixed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import Auth from '@axioms/web-js';
-import { environment } from '../environments/environment';
+import { Auth } from '@axioms/web-js';
 import config from '../../config.json';
 
 @Injectable({providedIn: 'root'})
