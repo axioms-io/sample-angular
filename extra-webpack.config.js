61c1c1c48932721692b2b905979e52bc979b82f1
Abhishek Tiwari <abhishek.twr@gmail.com>
2020-04-01 14:06:06 +1100

    Backup env variable

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         new Dotenv({
             path: './.env', // Path to .env file (this is the default)
             safe: true // load .env.example (defaults to "false" which does not use dotenv-safe)
-        })
+        }),
+        new webpack.EnvironmentPlugin(['NG_APP_Client_Id', 'NG_APP_Axioms_Domain', 'NG_APP_Redirect_Uri', 'NG_APP_Post_Logout_Uri', 'NG_APP_Response_Type', 'NG_APP_Scope'])
     ]
 }
